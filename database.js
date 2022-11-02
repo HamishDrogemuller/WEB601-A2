60649181f238e402e04b443af2489156ae8e3c63
HamishDrogemuller <hamish-drogemuller@live.nmit.ac.nz>
2022-11-01 17:13:06 -0700

    Databse connection JS

---
+++ database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+module.exports = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Corvere300',
+        database: 'Web601'
+    }
+)
